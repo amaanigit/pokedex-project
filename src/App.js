3e11f8c2d81dbb6f29d543bd058b81dfb197a21a
Amaani Thassim <thassima@gmail.com>
2021-09-16 13:56:06 +0100

    tried starting basic funcitonality of loading the first 12 pokemons - in progress

--- src/App.js
+++ src/App.js
@@ -8,23 +8,21 @@ import backgroundPokemon from './assets/background-pokemon.svg'
 import HeaderNav from './components/HeaderNav';
 import PokedexPage from "./components/PokedexPage";
 import PartyPage from "./components/PartyPage";
+import PokemonList from "./components/PokemonList";
 
-const paginationIncrement = 12;
-const basePokemonApiUrl = 'https://pokeapi.co/api/v2/pokemon/' ;
+
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      genPokemon: null,
-      loadedCounter: paginationIncrement,
+      genPokemon: null
     }
   }
 
   componentDidMount() {
     this.fetchGenPokemon();
-    console.log(this);
   }
 
   // get the species of all gen 1 pokemon from the pokeAPI: https://pokeapi.co/ and update the component state
@@ -35,8 +33,8 @@ class App extends Component {
     .then(response => response.json())
     .then(data => this.setState({genPokemon: this.orderGenPokemon(data.pokemon_species)}))
     .catch(error => {
-      console.log('error fetching data from https://pokeapi.co/: ', error)
-    })
+      console.log('error fetching pokemon data: ', error)
+    });
   }
 
   // return an ordered list of gen 1 pokemon by ID, using the fetched data from the API call
@@ -59,16 +57,21 @@ class App extends Component {
 
     return pokemonIds;
   }
+
+
+
   
 
   render() {
     // any dymiac things that causes the app to get re rendered is done in here
 
+
     return (
       <div className="app">
-       
         <HashRouter>
           <HeaderNav/>
+
+          <PokemonList genPokemon={this.state.genPokemon}/>
           
           <div className="page-container">
           <Route path="/" exact component={PokedexPage}/>
@@ -84,40 +87,5 @@ class App extends Component {
   }
 }
 
-// function App() {
-//   // initialising the app state
-//   const [appState, setAppState] = useState({
-//     genPokemon: null
-//   });
-
-//   // getting the api for gen 1 pokemons
-//   useEffect(() => {
-//     const genApiUrl = 'https://pokeapi.co/api/v2/generation/1/';
-//     fetch(genApiUrl)
-//       .then((res) => res.json())
-//       .then((apiData) => {
-//         setAppState({ genPokemon: apiData.pokemon_species });
-//       }).catch(console.log('error fetching data from api'));
-      
-//   }, [setAppState]);
-  
-//   const pokemonIds = [];
-
-//   function extractAndAddId(item) {
-//     const idRegex = /\/([0-9]+)\//; // to get numbers between the / / in the url - should just be the IDs 
-//     let urlID = item.url.match(idRegex).pop();
-//     urlID = parseInt(urlID);
-//     return urlID;
-//   }
-
-//   // add and sort by IDs
-//   if(appState.genPokemon) {
-//     appState.genPokemon.map((item) => pokemonIds.push({ id: extractAndAddId(item), name: item.name}));
-//     pokemonIds.sort((a, b) => (a.id > b.id) ? 1 : -1);
-//     console.log(pokemonIds);
-//   }
-
-  
-// }
 
 export default App;
